7ec4a4128df81e2b2157db610ec5091570d0c916
Leonardo Donato <kqjut3@gmail.com>
2020-10-26 18:54:58 -0300

    adicionado escolha 6

--- projetoBiblioteca/main.py
+++ projetoBiblioteca/main.py
@@ -4,6 +4,8 @@ livro = dict()
 livros = list()
 continuar = " "
 voltarMenu = " "
+lista = []
+
 while continuar != "sair":
     voltarMenu = " "
     print("1 - adcionar livros\n2 - atualizar quantidade de livros\n3 - sair")
@@ -14,7 +16,7 @@ while continuar != "sair":
             livros.append(livro.copy())
             voltarMenu = str(input("Digite menu para voltar ao menu ou ENTER para continuar ").lower())
 
-    if escolha == 2:
+    elif escolha == 2:
         nome = str(input("Qual nome do livro que você quer mudar a quantidade?: "))
         valor = int(input("Novo valor de quantidade do livro: "))
         for l in livros:
@@ -22,14 +24,44 @@ while continuar != "sair":
                 l["quantidade"] = valor
                 print(f" O livro: {l['nome']} atualizou o valor para {l['quantidade']}")
 
-    if escolha == 3:
-        continuar = "sair"
-
-    if escolha == 4:
+    elif escolha == 3:
         nome = str(input("Qual nome do livro que você quer ver os dados?: "))
         for l in livros:
             if l["nome"] == nome:
                 print(f"O nome do livro é {l['nome']}\n"
                       f"Ele é {l['fisicoOuEletronico']}\n"
                       f"Lançado em {l['anoLancamento']}")
-print(livros)
+
+    elif escolha == 6:
+        escolha = str(input('Como você quer buscar o livro? [ano, titulo, autor, assunto] ').lower())
+        if escolha == 'ano':
+            ano = int(input('Digite o ano para a pesquisa: '))
+            for l in livros:
+                if l['anoLancamento'] == ano:
+                    lista.append(l['nome'])
+            print(lista)
+            lista.clear()
+        elif escolha == 'titulo':
+            titulo = input('Insira o titulo do livro: ').lower()
+            for l in livros:
+                if titulo in l['nome']:
+                    lista.append(l['nome'])
+            print(lista)
+            lista.clear
+        elif escolha == 'autor':
+            autor = input('Insira o autor do livro: ').lower()
+            for l in livros:
+                if autor in l['autor']:
+                    lista.append(l['nome'])
+            print(lista)
+            lista.clear
+        elif escolha == 'assunto':
+            assunto = input('Insira o assunto do livro: ').lower()
+            for l in livros:
+                if assunto in l['assunto']:
+                    lista.append(l['nome'])
+            print(lista)
+            lista.clear
+
+        
+        
